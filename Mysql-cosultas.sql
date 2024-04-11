43e363ae9e7f28152b73469f41fa3522aa3502b0
braina004 <braianemanueltito@gmail.com>
2024-04-11 19:47:17 -0300

    avg

--- Mysql-cosultas.sql
+++ Mysql-cosultas.sql
@@ -30,3 +30,28 @@ SELECT Club,SUM(Value_eur),MAX(Value_eur) FROM Players_description
 GROUP BY Club;
 -- COUNT conocer el total de valores
 SELECT COUNT(*) FROM Players_description;
+SELECT * FROM Players_description;
+-- sueldos mayores a 200
+SELECT Nationality,FirstName,Value_eur,COUNT(*)FROM Players_description
+WHERE Wage_eur>100
+GROUP BY Nationality,FirstName,Value_eur; 
+-- AND el apellido kien tengan menos de 30 años y posicion CB 
+SELECT*FROM Players_description;
+SELECT LastName,Age,Team_Position FROM Players_description
+WHERE Age > 30 AND Team_Position ='CB';
+-- IN seleciona las nacionalidad de arg,bra,fra
+SELECT * FROM Players_description
+WHERE Nationality IN('ARG','BRA','FRA');
+-- el promedio de altura de los jugadores mayores de 28 por nacionalidad
+SELECT Nationality, AVG(Height) FROM players_description
+WHERE Age > 28
+GROUP BY Nationality
+ORDER BY AVG(Height);
+-- el jugadores que menos gana por nacionalidad
+SELECT Nationality,MIN(Value_eur) FROM Players_description
+GROUP BY Nationality;
+
+SELECT Team_position,MAX(Height)FROM Players_description
+GROUP BY Team_position;
+
+-- HA
